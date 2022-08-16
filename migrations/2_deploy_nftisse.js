ef70a86d09999142ca72572f84bf0155072749aa
lza_menace <lza_menace@protonmail.com>
2022-08-16 01:08:11 -0700

    track separate rmutt address for different nets

--- migrations/2_deploy_nftisse.js
+++ migrations/2_deploy_nftisse.js
@@ -1,13 +1,16 @@
 var NFTisse = artifacts.require("NFTisse");
 
 module.exports = function(deployer) {
-  let addr;
+  let opensea;
+  let rmutt;
   if (deployer.network == 'mainnet') {
-    console.log('[+] Using OpenSea mainnet proxy address 0xa5409ec958c83c3f309868babaca7c86dcb077c1');
-    addr = '0xa5409ec958c83c3f309868babaca7c86dcb077c1';
+    opensea = '0xa5409ec958c83c3f309868babaca7c86dcb077c1';
+    rmutt = '0x6c61fB2400Bf55624ce15104e00F269102dC2Af4';
+    console.log(`[+] Using OpenSea mainnet proxy address ${opensea} and RMUTT address ${rmutt}`);
   } else {
-    console.log('[+] Using OpenSea testnet proxy address 0xf57b2c51ded3a29e6891aba85459d600256cf317');
-    addr = '0xf57b2c51ded3a29e6891aba85459d600256cf317';
+    opensea = '0xf57b2c51ded3a29e6891aba85459d600256cf317';
+    rmutt = '0x471365176f83e055d70e050e28d5c654651f12f9';
+    console.log(`[+] Using OpenSea testnet proxy address ${opensea} and RMUTT address ${rmutt}`);
   }
-  deployer.deploy(NFTisse, addr, "0x471365176f83e055d70e050e28d5c654651f12f9");
+  deployer.deploy(NFTisse, opensea, rmutt);
 };
